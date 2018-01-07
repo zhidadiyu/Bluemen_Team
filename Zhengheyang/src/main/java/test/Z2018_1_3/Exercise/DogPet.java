98ab07d8b45308cb2223a42fd2a7553aaf50489c
zhidadiyu <zhi_da_di_yu@163.com>
2018-01-07 20:36:37 +0800

    case

--- Zhengheyang/src/main/java/test/Z2018_1_3/Exercise/DogPet.java
+++
@@ -1,8 +0,0 @@
-package test.Z2018_1_3.Exercise;
-
-public class DogPet implements PetRestaurant {
-    @Override
-    public void buy() {
-        System.out.println("我是猫，需要狗粮");
-    }
-}
--- Zhengheyang/src/main/java/test/Z2018_1_3/Exercise/PetRestaurant.java
+++
@@ -1,8 +0,0 @@
-package test.Z2018_1_3.Exercise;
-
-/*
- 申明一个接口
- */
-public interface PetRestaurant {
-    public void buy();
-}
